428aa14e94eb18264c42163475931c2bc188bbb6
NadineTsianta <k.tsianta@rocketmail.com>
2024-01-15 09:58:35 +0000

    moved questions in a new js file

--- starter/assets/js/codeQuiz.js
+++ starter/assets/js/codeQuiz.js
@@ -1,4 +1,3 @@
-// A start button that when clicked a timer starts and the first question appears.
 var userHighscoreInput = document.querySelector('#initials');
 var startBtnEl = document.querySelector('#start');
 var submitBtnEl = document.querySelector('#submit');
@@ -12,34 +11,7 @@ var endScreen = document.querySelector("#end-screen");
 var finalScore = document.querySelector("#final-score");
 
 
-
-
-// Questions contain buttons for each answer.
-
-var question1 = {
-
-    question: "Inside which HTML element do we put the JavaScript?",
-    options: ["<script>", "Javascript"],
-    correctAnswer: "'<script>'"
-};
-
-var question2 = {
-    question: "Which is the correct place to insert a JavaScript?",
-    options: ["The <body> section", "The  <head> section"],
-
-    correctAnswer: "The <body> section"
-}
-
-var question3 = {
-    question: "What is the correct syntax for referring to an external script called 'xxx.js' ?",
-    options: ["<script href='xxx.js'", "<script src='xxx.js'"],
-
-    correctAnswer: "<script src='xxx.js'"
-
-}
-
-
-
+// A start button that when clicked a timer starts and the first question appears.
 
 
 
@@ -49,11 +21,9 @@ var question3 = {
 
 // The quiz should end when all questions are answered or the timer reaches 0.
 
-startBtnEl.addEventListener("click", quizQuestions)
+startBtnEl.addEventListener("click", createQuiz);
 
-//  Capture user Initials -> Submit button
-submitBtnEl.addEventListener("click", function (event) {
-
-})
 
 // When the game ends, it should display their score and give the user the ability to save their initials and their score
+//  Capture user Initials -> Submit button
+submitBtnEl.addEventListener("click", finalScore )
---
+++ starter/assets/js/score.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        question:"Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "<script>", correct: true },
+            {text: "Javascript", correct: false}
+        ]
+    },
+
+    {
+        question: "Which is the correct place to insert a JavaScript?", 
+        answers: [
+            {text:"The <body> section", correct: true},
+            {text:"The  <head> section", correct: false}
+        ]
+    },
+    {
+        question:"What is the correct syntax for referring to an external script called 'xxx.js' ?",
+        answers: [
+            {text:"<script href='xxx.js'", correct: false},
+            {text:"<script src='xxx.js'", correct: true}
+        ]
+    }
+]

7b1f08652a6e02af8b5b7ba7175d1847e190471f
NadineTsianta <k.tsianta@rocketmail.com>
2024-01-15 13:33:03 +0000

    fixed issue with score function and how to update | still not displaying on the screen

--- starter/assets/js/score.js
+++ starter/assets/js/score.js
@@ -1,13 +1,14 @@
 let score = 0;
 
 function updateScore() {
-    let userOption = // Get the user's selected option, you need to obtain this value in your code.
+    let userOption = target.textContent;// Get the user's selected option
     let correctAnswer = questions[currentQuestionIndex].answers.find(answer => answer.correct).text;
 
     if (userOption === correctAnswer) {
         score++;
     } else {
         score--;
+        
     }
 
     // Ensure the score doesn't go below zero
